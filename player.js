eba5f296c1dcf0b9d5b96c362429d10b8a68d3dd
kolyan173 <kolyan173@gmail.com>
2015-02-13 23:02:54 +0200

    getStreamWord got promise

--- player.js
+++ player.js
@@ -3,7 +3,9 @@ var fs = require('fs'),
 	Speaker = require('speaker'),
 	request = require('request'),
 	Promise = require('promise'),
+	needle = require('needle'),
 	Q = require('q'),
+	rp = require('request-promise'),
 	CombinedStream = require('combined-stream'),
 	 _ = require('lodash');
 
@@ -27,7 +29,7 @@ Generator.prototype = {
 	wordAudio: function(word, language) {
 		var res = 'http:\//translate.google.com/translate_tts?ie=UTF-8&' + 
 			'q=' + (word || this.words[0]) + '&tl=' + (language || 'en') ;
-		console.log(res);
+		// console.log(res);
 		return res;
 	},
 	translate: function(sourceText, sourceLang, targetLang) {
@@ -35,47 +37,54 @@ Generator.prototype = {
 			(sourceLang || 'en') + '&tl=' + (targetLang || 'ru') + '&dt=t&q=' + (sourceText || 'word to translate');
 	},
 	getTranslatedWord: function() {
-		var self = this;
-		var deferred = Q.defer();
-		request(this.translate(getAttributes().word), function(err, res, body) {
-			if(err) deferred.reject(new Error(err));
-
-			deferred.resolve(JSON.parse(body.replace(/,,/g, ','))[0][0][0]);
-		}.bind(this));
-		return deferred.promise;
+		return rp(this.translate(getAttributes().word))
+			.then(function(body) {
+				return JSON.parse(body.replace(/,,/g, ','))[0][0][0];
+			});
 	},
 	getStreamWord: function(play, word, language) {
 		var word = word || '';
 		var deferred = Q.defer();
 		request.get( this.wordAudio(word, language) )
-		.on('response', function(stream) {
-			console.log('getStreamWord', stream);
-			// console.log('getStreamWord', word);
-			deferred.resolve(stream);
-			// return !play && stream || stream.pipe(new lame.Decoder())
-			// 	.on('format', function (format) {
-			// 		this.pipe(new Speaker(format));
-			// 	});
-		});	
+			.on('response', function(stream) {
+				deferred.resolve(stream.pipe(new lame.Decoder()));
+				// return !play && 'stream' || stream.pipe(new lame.Decoder())
+				// 	.on('format', function (format) {
+				// 		this.pipe(new Speaker(format));
+				// 	});
+			});	
 		return deferred.promise;
 	},
 	buildPaire: function() {
-		var stream = this.getStreamWord(getAttributes().play);
+		// var stream = this.getStreamWord(getAttributes().play);
+		return this.getStreamWord(getAttributes().play)
+			.then(function(stream) {
+				stream.on('format', function (format) {
+					this.pipe(new Speaker(format));
+				});
+				return 
+			}.bind(this))
+			.then(function(stream2) {
 
+			});
 		return Promise.all([
 				this.getTranslatedWord(),
-				stream
+				this.getStreamWord(getAttributes().play)
 			]).then(function(res) {
-				// console.log(res)
-				var stream2 = this.getStreamWord(getAttributes().play, res[1], 'ru');
+				console.log(res);
+				res[1].on('format', function (format) {
+						this.pipe(new Speaker(format));
+					});
+				var stream2 = this.getStreamWord(getAttributes().play, res[0], 'ru');
 				return [res[0], stream2]; 
 			}.bind(this)).then(function(res) {
-				var combinedStream = CombinedStream.create();
+				// var combinedStream = CombinedStream.create();
+				var combinedStream = res[0];
 				
-				combinedStream.append(res[0]);
-				combinedStream.append(res[1]);
+				// combinedStream.append(res[0]);
+				// combinedStream.append(res[1]);
 
-				return combinedStream.pipe(new lame.Decoder())
+				return combinedStream/*2*/
 					.on('format', function (format) {
 						this.pipe(new Speaker(format));
 					});
